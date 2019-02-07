bc0634bfe45d137828f18df52109f7506189da8a
max.cian <chutarojian@gmail.com>
2019-02-07 20:53:28 +0800

    feat(SPEC-G1): implement SPEC-G1

--- README.md
+++ README.md
@@ -3,17 +3,33 @@
 InertIA，為 Inert Investment Assistant 的縮寫，是一跟指數化投資人一樣懶惰的投資助理，希冀協助指數型投資人能夠不受到外力的影響(消息、短期波動)，保持著嚴謹的投資慣性(inertia)。
 
 # SPEC
-**backend**
+**backend (prefix B)**
 
 1. read Fistrade standard trading history output file (CSV), write to sqlite3
 2. given a symbol "ticker" and "price", calculate that symbol's annually returns
     - 2.1 support multiple tickers calculation
 
+**GUI (prefix G)**
+1. add a window for providing a input interface to symbol and price and calcualting annualized return by input information
+2. add a window for providing a entry point for selecting file and import it
 
 # Dependencies
 1. docopt
 2. sqlalchemy
 3. scipy-1.2.0 (numpy-1.16.1)
+4. PyQt5-5.11.3
 
 # Online resource
 - [Alpha Vantage](https://www.alphavantage.co/): free APIs for realtime and historical data on stocks and etc. 
+
+
+# Good Q&A worth to note (will move to other place after finishing development)
+- [dynamically adding and removing widgets in PyQt](https://stackoverflow.com/questions/8651742/dynamically-adding-and-removing-widgets-in-pyqt)
+- [setFocus() after show](https://stackoverflow.com/questions/49418905/pyqt-setting-focus-on-qlineedit-widget)
+- [How to restrict user input in QLineEdit in pyqt](https://stackoverflow.com/questions/15829782/how-to-restrict-user-input-in-qlineedit-in-pyqt)
+    - base: [QValidator](http://pyqt.sourceforge.net/Docs/PyQt4/qvalidator.html)
+    ```python
+    self.onlyInt = QIntValidator()
+    self.LineEdit.setValidator(self.onlyInt)
+    ```
+- [Validating user input in PyQt4(it work on PyQt5) using QValidator](https://snorfalorpagus.net/blog/2014/08/09/validating-user-input-in-pyqt4-using-qvalidator/)
---
+++ sources/menu.py
@@ -0,0 +1,203 @@
+import os
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QLabel, QPushButton, QLineEdit
+from PyQt5.QtGui import QValidator, QDoubleValidator
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QFormLayout
+from PyQt5.QtWidgets import QStyleFactory
+from PyQt5.QtWidgets import QTextBrowser
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem, QAbstractScrollArea
+from financial import financial_func
+
+PriceValidator = QDoubleValidator()
+PriceValidator.setRange(0.000000, 9999.000000, 6)
+PriceValidator.setNotation(QDoubleValidator.StandardNotation)
+
+class PriceLineEdit(QLineEdit):
+    '''
+    inspired by : https://snorfalorpagus.net/blog/2014/08/09/validating-user-input-in-pyqt4-using-qvalidator/
+    '''
+    def __init__(self, *argv, **kwargv):
+        super(self.__class__, self).__init__(*argv, **kwargv)
+        self.setValidator(PriceValidator)
+        self.textChanged.connect(self._check_state)
+        self.textChanged.emit(self.text())
+
+    def _check_state(self):        
+        validator = self.validator()
+        if validator:
+            state = validator.validate(self.text(), 0)[0]
+            if state == QValidator.Acceptable:
+                color = '#c4df9b' # green
+            elif state == QValidator.Intermediate:
+                color = '#f6989d' # red
+            else:
+                color = '#fff79a' # yellow
+            self.setStyleSheet('QLineEdit { background-color: %s }' % color)
+
+    def isTextValid(self):
+        validator = self.validator()
+        if validator:
+            state = validator.validate(self.text(), 0)[0]
+            if state == QValidator.Acceptable:
+                return True
+            else:
+                return False
+        return True
+
+class PopWarning(QMessageBox):
+    def __init__(self, msg, detail=None, *argv, **kwargv):
+        super(self.__class__, self).__init__(*argv, **kwargv)
+        self._setMsgs(msg, detail)
+
+    def _setMsgs(self, msg, detail):
+        self.setIcon(QMessageBox.Warning)
+        self.setWindowTitle("Warning")
+        # self.setInformativeText(msg)
+        self.setText(msg)
+        if detail:
+            self.setDetailedText(detail)
+        self.setStandardButtons(QMessageBox.Ok)
+    #     self.buttonClicked.connect(self._msgbtn)
+    # def _msgbtn(self):
+    #     print("Button pressed is: {}".format(self.text()))
+
+class AnnualizedReturnCalculator(QWidget):
+    def __init__(self, *argv, **kwargv):
+        super(self.__class__, self).__init__(*argv, **kwargv)
+        self.uuid = 0
+        self.setUI()
+
+    @property
+    def _getNextUUID(self):
+        self.uuid += 1
+        return self.uuid
+
+    def setFocus(self):
+        for uuid in self.symbolPriceRow:
+            symbol, price, action = self.symbolPriceRow[uuid]
+            if action is None:
+                symbol.setFocus()
+                break
+
+    def setUI(self):
+        # initialization
+        self.setWindowTitle("Annualized Return Calculator")
+        
+        line_symbol = QLineEdit()
+        line_symbol.setFocus()        
+        line_symbol.setPlaceholderText('e.g. VT')
+        line_price = PriceLineEdit()
+        line_price.setPlaceholderText('e.g. 71.06')
+    
+        self.symbolPriceRow = { self._getNextUUID: (line_symbol, line_price, None) }        
+        self.addRow = QPushButton("&Add")
+        self.ok = QPushButton("&OK")
+        self.clear = QPushButton("&Clear")
+
+        # layouts
+        self.table_layout = QVBoxLayout()
+        # self.text_result_browser = QTextBrowser()
+
+        self.form_layout = QFormLayout()
+        self.form_layout.addRow(QLabel("Symbol"), QLabel("Price"))
+        self.form_layout.addRow(line_symbol, line_price) 
+        self.action_layout = QFormLayout()
+        self.action_layout.addRow(QLabel("Action"))
+        blank_btn = QPushButton("")
+        blank_btn.setFlat(True)
+        self.action_layout.addRow(blank_btn)
+        hbox_layout = QHBoxLayout()
+        hbox_layout.addLayout(self.form_layout)
+        hbox_layout.addLayout(self.action_layout)
+
+        self.vbox_layout = QVBoxLayout()
+        self.vbox_layout.addLayout(self.table_layout)
+        # self.vbox_layout.addWidget(self.text_result_browser)
+        self.vbox_layout.addLayout(hbox_layout)
+        self.vbox_layout.addWidget(self.addRow)
+        self.vbox_layout.addWidget(self.ok)
+        self.vbox_layout.addWidget(self.clear)
+        self.setLayout(self.vbox_layout) # finally
+
+        # add signal and slot
+        self.clear.clicked.connect(self._clearAllLine)
+        self.ok.clicked.connect(self._getUserInput)
+        self.addRow.clicked.connect(self._addNewSymbolPriceRow)
+
+    def _clearAllLine(self):
+        for uuid in self.symbolPriceRow:
+            symbol, price, action = self.symbolPriceRow[uuid]
+            symbol.clear()
+            price.clear()
+            
+    def _addNewSymbolPriceRow(self):
+        line_symbol = QLineEdit()
+        line_price = PriceLineEdit()
+        delete_line = QPushButton("&Del")
+        self.form_layout.addRow(line_symbol, line_price, )
+        self.action_layout.addRow(delete_line)
+        _uuid = self._getNextUUID
+        self.symbolPriceRow.update({
+            _uuid: (line_symbol, line_price, delete_line)
+        })
+        def _delete_line(uuid):
+            def _del():
+                symbol, price, action = self.symbolPriceRow.pop(uuid)
+                symbol.deleteLater()
+                price.deleteLater()
+                action.deleteLater()
+            return _del
+        delete_line.clicked.connect(_delete_line(_uuid))
+    
+    def _check_symbol_price(self, symbol, price):
+        symbol_text = symbol.text()        
+        if not symbol_text:
+            raise ValueError('Found blank symbol, please fill it.')
+        if len(symbol_text) > 128:
+            raise ValueError('Is it possible that number of characters of stock ticker symbol would larger than 128?')
+        if not price.text():
+            raise ValueError('Found blank price, please fill it.')
+        if not price.isTextValid():
+            raise ValueError('Found invalid number of price ({})'.format(price.text()))
+
+    def _getUserInput(self):
+        symbol_to_price = dict()
+        try:
+            for uuid in self.symbolPriceRow:
+                symbol, price, action = self.symbolPriceRow[uuid]
+                self._check_symbol_price(symbol, price)
+                if symbol_to_price.get(symbol.text()):
+                    raise KeyError('Symbol "{}" occurs twice! Please check your input again.'.format(symbol.text()))
+                symbol_to_price.update({ symbol.text().strip().lower(): float(price.text())})
+        except Exception as err:
+            pop = PopWarning(msg=str(err))
+            pop.exec_()
+            print(err)
+        else:
+            ar = financial_func.calcMultiAnnualizedReturn(symbol_to_price)
+            table = QTableWidget(2, 2)
+            
+            label = QTableWidgetItem("Annualized Return (%)")
+            portfolio = QTableWidgetItem("Portfolio")
+            portfolio_return = QTableWidgetItem("{:.2f}".format(ar * 100))
+            table.setItem(0, 1, label)
+            table.setItem(1, 0, portfolio)
+            table.setItem(1, 1, portfolio_return)
+            for i in reversed(range(self.table_layout.count())): 
+                self.table_layout.itemAt(i).widget().deleteLater()
+            table.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
+            table.resizeColumnsToContents()
+            table.resizeRowsToContents()
+            self.table_layout.addWidget(table)
+            
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyle('Macintosh')
+    w = AnnualizedReturnCalculator()
+    w.show()
+    w.setFocus()
+    sys.exit(app.exec_())
+
+
