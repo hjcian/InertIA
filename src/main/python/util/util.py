cf740666cd512a20f089a590b754b2100ff39eda
max.cian <chutarojian@gmail.com>
2019-02-09 14:17:21 +0800

    fix(load config): change config loading behavior

--- src/main/python/util/util.py
+++ src/main/python/util/util.py
@@ -1,11 +1,23 @@
+import os
 import sys
 import json
+import traceback
+
+DEFAULT_CONFIG = {
+    "database": "ft.db",
+}
 
 def loadConfig(fpath):
     try:
-        with open(fpath, 'r') as fp:
-            config = json.load(fp)
-            return config
-    except Exception as err:
-        print('Error when loading config.json ({})'.format(err))
-        sys.exit(1)
+        if os.path.isfile(fpath):
+            with open(fpath, 'r') as fp:
+                config = json.load(fp)
+                return config
+        else:
+            os.makedirs(os.path.dirname(fpath))
+            with open(fpath, 'w') as fp:
+                json.dump(DEFAULT_CONFIG, fp)
+            return DEFAULT_CONFIG
+    except Exception:
+        print('Error when loading/writing config ({})'.format(traceback.format_exc()))
+        return DEFAULT_CONFIG
